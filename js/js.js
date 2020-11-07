694570a8d2d5ce83ed4d1c3b9792a80be3206acd
sCerdan <santyjoell@gmail.com>
2020-11-07 20:05:17 -0300

    Base del nuevoProyectoFinal

---
+++ js/js.js
@@ -0,0 +1,16 @@
+//FUNCION btn
+document.getElementById('btn').addEventListener('click', traerdatos);
+let img = document.getElementById('img');
+function traerdatos() {
+    //console.log('activamos la ft');
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', './json/cartas.json', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let cardE = JSON.parse(this.responseText);
+            //console.log(cardE[0].image_url);
+            img.setAttribute('src', cardE[0].image_url)
+        }
+    }
+};
