85a8bc658c71b81de802944730a011503c683c21
sCerdan <santyjoell@gmail.com>
2020-11-07 23:10:49 -0300

    Se agregan cartasNpc

--- js/js.js
+++ js/js.js
@@ -1,6 +1,31 @@
-//FUNCION btn
+//Se tiene que registrar usuario
+
+//Se tiene que loggear usuario
+
+//Debe elegir carta
+let primerCarta = prompt('Elija su primer carta');
+traerdatosUser();
+//FUNCION btn Npc
+let imgg = document.getElementById('imgI');
+function traerdatosUser() {
+    //console.log('activamos la ft');
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', './json/cartas.json', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let cardE = JSON.parse(this.responseText);
+            //Atributo del ID del elemento
+            imgI.setAttribute('src', cardE[primerCarta].image_url);
+            console.log(cardE[primerCarta].atk);
+        }
+    }
+};
+
+
+//FUNCION btn Npc
 document.getElementById('btn').addEventListener('click', traerdatos);
-let img = document.getElementById('img');
+let img = document.getElementById('imgD');
 function traerdatos() {
     //console.log('activamos la ft');
     const xhttp = new XMLHttpRequest();
@@ -9,9 +34,13 @@ function traerdatos() {
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             let cardE = JSON.parse(this.responseText);
-            //console.log(cardE[0].image_url);
-            img.setAttribute('src', cardE[22].image_url);
-            console.log(cardE[22].atk)
+            console.log(num);
+            var num = NumerosAleatorios(0, 23);
+            //Atributo del ID del elemento
+            let cartaNpc = imgD.setAttribute('src', cardE[num].image_url);
         }
     }
 };
+function NumerosAleatorios(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+ }
