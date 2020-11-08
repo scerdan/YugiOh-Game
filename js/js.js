d997c29c8d1a86a68a94b38c21995262df92d666
sCerdan <santyjoell@gmail.com>
2020-11-08 17:42:52 -0300

    Se agrega logica precaria

--- js/js.js
+++ js/js.js
@@ -3,44 +3,51 @@
 //Se tiene que loggear usuario
 
 //Debe elegir carta
-let primerCarta = prompt('Elija su primer carta');
+primerCarta = prompt('Elija su primer carta');
 console.log(primerCarta);
 
-//FUNCION btn Npc
+//FUNCION btn User
 let imgg = document.getElementById('imgI');
+traerdatosUser();
+
 function traerdatosUser() {
     //console.log('activamos la ft');
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?type=XYZ%20Monster', true);
     xhttp.send();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
             let cardE = JSON.parse(this.responseText);
             //Atributo del ID del elemento
-            imgD.setAttribute('src', cardE.data[primerCarta].card_images[0].image_url);
-            console.log(cardE.data[primerCarta].card_images[0].image_url);
+            imgI.setAttribute('src', cardE.data[primerCarta].card_images[0].image_url);
+            // console.log(cardE.data[primerCarta].card_images[0].image_url);
+            atkUser = cardE.data[primerCarta].atk;
+            console.log('El ataque de su carta es: ' + atkUser);
         }
     }
 };
 
 //FUNCION btn Npc
 let img = document.getElementById('imgD');
+
 function traerdatos() {
     //console.log('activamos la ft');
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?type=XYZ%20Monster', true);
     xhttp.send();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
             let cardE = JSON.parse(this.responseText);
-            var num = NumerosAleatorios(1, 420);
+            let num = NumerosAleatorios(1, 420);
             //Atributo del ID del elemento
             imgD.setAttribute('src', cardE.data[num].card_images[0].image_url);
-            console.log(cardE.data[num].card_images[0].image_url);
+            atkNpc = cardE.data[num].atk;
+            console.log('El ataque de su oponente es: ' + atkNpc);
         }
     }
 };
 
 function NumerosAleatorios(min, max) {
     return Math.round(Math.random() * (max - min) + min);
- }
+}
+
