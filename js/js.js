8e044d272af0d87b0c8d4231a5345846c2cb88b6
sCerdan <santyjoell@gmail.com>
2020-11-08 17:15:46 -0300

    Se agrega la consulta API

--- js/js.js
+++ js/js.js
@@ -4,37 +4,39 @@
 
 //Debe elegir carta
 let primerCarta = prompt('Elija su primer carta');
-traerdatosUser();
+console.log(primerCarta);
+
 //FUNCION btn Npc
 let imgg = document.getElementById('imgI');
 function traerdatosUser() {
     //console.log('activamos la ft');
     const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', './json/cartas.json', true);
+    xhttp.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?type=XYZ%20Monster', true);
     xhttp.send();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             let cardE = JSON.parse(this.responseText);
             //Atributo del ID del elemento
-            imgI.setAttribute('src', cardE[primerCarta].image_url);
-            console.log(cardE[primerCarta].atk);
+            imgD.setAttribute('src', cardE.data[primerCarta].card_images[0].image_url);
+            console.log(cardE.data[primerCarta].card_images[0].image_url);
         }
     }
 };
+
 //FUNCION btn Npc
 let img = document.getElementById('imgD');
 function traerdatos() {
     //console.log('activamos la ft');
     const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', './json/cartas.json', true);
+    xhttp.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?type=XYZ%20Monster', true);
     xhttp.send();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             let cardE = JSON.parse(this.responseText);
-            var num = NumerosAleatorios(1, 23);
+            var num = NumerosAleatorios(1, 420);
             //Atributo del ID del elemento
-            imgD.setAttribute('src', cardE[num].image_url);
-            console.log(cardE[num].atk);
+            imgD.setAttribute('src', cardE.data[num].card_images[0].image_url);
+            console.log(cardE.data[num].card_images[0].image_url);
         }
     }
 };
