4ddf4f6b669318b743e2cdff67b39f65ece2dbb9
sCerdan <santyjoell@gmail.com>
2020-11-10 02:39:29 -0300

    Se agrega una logica precaria del juego

--- js/js.js
+++ js/js.js
@@ -2,14 +2,13 @@
 function enviar() {
     let correoUsuario = document.getElementById("mail").value;
     let passUsuario = document.getElementById("pass").value;
-
     if (correoUsuario == '' || passUsuario == '') {
         console.log("Debe ingresar un correo o pass validos");
     } else {
         sessionStorage.setItem('Mail', correoUsuario);
         sessionStorage.setItem('Password', passUsuario);
         console.log('Se ha registrado correctamente');
-  }
+    }
 };
 //Se loguea los usuarios in sessionStorage
 function acceso() {
@@ -23,7 +22,6 @@ function acceso() {
         console.log('Debe estar registrado para poder Ingresar:');
     }
 };
-
 //Logout
 function logout() {
     sessionStorage.clear();
@@ -32,7 +30,6 @@ function logout() {
 };
 //FUNCION btn User
 function traerdatosUser() {
-    //console.log('activamos la ft');
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?type=XYZ%20Monster', true);
     xhttp.send();
@@ -42,28 +39,37 @@ function traerdatosUser() {
             let primerCarta = NumerosAleatorios(1, 420);
             //Atributo del ID del elemento
             imgI.setAttribute('src', cardE.data[primerCarta].card_images[0].image_url);
-            // console.log(cardE.data[primerCarta].card_images[0].image_url);
-            atkUser = cardE.data[primerCarta].atk;
-            console.log('El ataque de su carta es: ' + atkUser);
+            nameCardUser = cardE.data[primerCarta].name;
+            atkCardUser = cardE.data[primerCarta].atk;
+            defCardUser = cardE.data[primerCarta].def;
+            console.log('El ataque de su carta es: ' + atkCardUser);
+            saveDatosUser();
         }
     }
 };
 //FUNCION btn Npc
-let img = document.getElementById('imgD');
-
 function traerdatos() {
-    //console.log('activamos la ft');
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php?type=XYZ%20Monster', true);
     xhttp.send();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            let cardE = JSON.parse(this.responseText);
+            cardE = JSON.parse(this.responseText);
             let num = NumerosAleatorios(1, 420);
             //Atributo del ID del elemento
             imgD.setAttribute('src', cardE.data[num].card_images[0].image_url);
-            atkNpc = cardE.data[num].atk;
-            console.log('El ataque de su oponente es: ' + atkNpc);
+            nameCardNpc = cardE.data[num].name;
+            atkCardNpc = cardE.data[num].atk;
+            defCardNpc = cardE.data[num].def;
+            console.log('El ataque de su oponente es: ' + atkCardNpc);
+            saveDatosNpc();
+        }
+        if (datosUser[1] > datosNPC[1]) {
+            console.log('Ha ganado!')
+        } else if (datosUser[1] < datosNPC[1]) {
+            console.log('Lo siento, Ha perdido!')
+        } else {
+            console.log('Empate!')
         }
     }
 };
@@ -71,3 +77,16 @@ function traerdatos() {
 function NumerosAleatorios(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
+// Datos de cartas seleccionadas
+let datosNPC = [];
+let datosUser = [];
+
+function saveDatosNpc() {
+    datosNPC.push(nameCardNpc, atkCardNpc, defCardNpc);
+    console.log(datosNPC);
+};
+
+function saveDatosUser() {
+    datosUser.push(nameCardUser, atkCardUser, defCardUser);
+    console.log(datosUser);
+};
